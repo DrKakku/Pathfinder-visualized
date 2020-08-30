1660b958c1b4f35915e5ff2ad3dfd46d9d8a2844
Yash Tripathi <Drkakku19@gmail.com>
2020-08-30 13:24:01 +0530

    kinda working

---
+++ pathfinding-visualised/src/components/todo.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Todos from "./todos";
+///import components
+
+
+const Todo = () =>
+{   return(
+    <div className="todo-container">
+    <ul className="todo-list"></ul>
+    <Todos />
+    <Todos />
+    <Todos />
+    <Todos />
+  </div>
+  );
+}
+export default Todo;
